53545152fd8665553213e6682da78d28c6327c18
Jens Hjalmarsson <jens.hjalmarsson@gmail.com>
2017-04-26 22:39:40 +0200

    Small revamp allowing of reaccuring index & keys

--- formata.js
+++ formata.js
@@ -13,15 +13,8 @@
     var s = '', type = typeof this, args = Array.prototype.slice.call(arguments);
     if(type == 'string') s = this.toString();
     else if(args.length > 0) s = args.shift();
-    var loopArray = function(args){
-      for(var k in args) s = s.replace('{'+ (Number(k)+1) +'}', args[k])
-    };
-    if(args.length === 1 && args[0].constructor !== String){
-      var args = args[0],
-          t = args.constructor;
-      if(t === Object) for(var k in args) s = s.replace('{'+ (k) +'}', args[k]);
-      else if(t === Array) loopArray(args);
-    } else loopArray(args);
+    if(args.length === 1 && args[0].constructor !== String) args = args[0];
+    for(var k in args) s = s.split('{'+ ((args.constructor === Array) ? Number(k)+1 : k) +'}').join(args[k]);
     return s;
   };
 
